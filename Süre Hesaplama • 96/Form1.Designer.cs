db1663104d2710d22b9fefae3937abf586e4ae43
halley <130781977+mshelovee@users.noreply.github.com>
2024-01-20 15:42:05 +0300

    Add files via upload

---
+++ Süre Hesaplama • 96/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Süre_Hesaplama___96
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtSure = new System.Windows.Forms.TextBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.lblFark = new System.Windows.Forms.Label();
+            this.tmrSure = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // txtSure
+            // 
+            this.txtSure.Location = new System.Drawing.Point(34, 57);
+            this.txtSure.Name = "txtSure";
+            this.txtSure.Size = new System.Drawing.Size(177, 22);
+            this.txtSure.TabIndex = 0;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHesapla.Location = new System.Drawing.Point(34, 85);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(177, 57);
+            this.btnHesapla.TabIndex = 1;
+            this.btnHesapla.Text = "HESAPLA";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // lblFark
+            // 
+            this.lblFark.AutoSize = true;
+            this.lblFark.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblFark.Location = new System.Drawing.Point(29, 182);
+            this.lblFark.Name = "lblFark";
+            this.lblFark.Size = new System.Drawing.Size(43, 29);
+            this.lblFark.TabIndex = 2;
+            this.lblFark.Text = "***";
+            // 
+            // tmrSure
+            // 
+            this.tmrSure.Interval = 1000;
+            this.tmrSure.Tick += new System.EventHandler(this.tmrSure_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(260, 281);
+            this.Controls.Add(this.lblFark);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.txtSure);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSure;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.Label lblFark;
+        private System.Windows.Forms.Timer tmrSure;
+    }
+}
+
---
+++ Süre Hesaplama • 96/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Süre_Hesaplama___96
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            tmrSure.Start();
+        }
+
+        private void tmrSure_Tick(object sender, EventArgs e)
+        {
+            string girisZamani = DateTime.Now.ToLongTimeString();
+            string cikisZamani = txtSure.Text;
+            TimeSpan girisCikisFarki = DateTime.Parse(cikisZamani).Subtract(DateTime.Parse(girisZamani));
+            string calismaSuresi = girisCikisFarki.ToString();
+            lblFark.Text = calismaSuresi.ToString();
+        }
+    }
+}
